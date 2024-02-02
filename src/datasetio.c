4129ecce27d1fca20ec7aaa6a7cab2f91233b0ce
psantro <pablosantron@gmail.com>
2024-02-02 14:20:38 +0100

    Added datasetio.c

---
+++ src/datasetio.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdio.h>
+#include "datasetio.h"
+
+int knn_load_dataset(char const *pathname, knn_dataset *dataset)
+{
+    return 0;
+}
+
+int knn_save_dataset(char const *pathname, knn_dataset const *dataset)
+{
+    return 0;
+}
