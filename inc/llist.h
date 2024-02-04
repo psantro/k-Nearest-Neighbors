0b04beabd428b9c137889642c725a0292cb8b9de
Antonio Garrido Masse <agarmas03@gmail.com>
2024-02-04 13:09:07 +0100

    Llist update 2

--- inc/llist.h
+++ inc/llist.h
@@ -1,4 +1,14 @@
-typedef struct node{
+#ifndef KNN_DATASETIO_H
+#define KNN_DATASETIO_H
+
+/**
+ * @brief Linked list
+ */
+
+typedef struct node
+{
     float data[NHOURS];
-    struct Nodo* next;
+    struct Nodo *next;
 } node;
+
+#endif
