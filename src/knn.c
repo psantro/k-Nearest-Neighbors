85def097426bc509a622cc9e4373764b47fc8934
Antonio Garrido Masse <agarmas03@gmail.com>
2024-02-06 12:55:08 +0100

    knn join

---
+++ src/knn.c
@@ -0,0 +1,103 @@
+#include <assert.h>
+#include <stdio.h>
+#include <math.h>
+#include "knn.h"
+#include "llist.h"
+
+float calculate_distance(float *neighbor, float *base)
+{
+    float total_distance = 0.0;
+
+    for (int hour = 0; hour < NHOURS; hour++)
+    {
+        total_distance += fabs(neighbor[hour] - base[hour]);
+    }
+
+    return total_distance;
+}
+
+knn_kllist find_k_nearest_neighbors(knn_dataset dataset, size_t base, size_t k)
+{
+    knn_kllist neighbors;
+    neighbors->first = NULL;
+    neighbors->size = 0;
+
+    float *base_measurements = dataset->table[base];
+    float *temp_measurements;
+    float distance;
+
+    for (size_t i = 0; i < dataset->ndays; i++)
+    {
+        if (i != base)
+        {
+            temp_measurements = dataset->table[i];
+            distance = calculate_distance(temp_measurements, base_measurements);
+            insert_sorted(&neighbors, distance, i);
+
+            if (neighbors->size > k)
+            {
+                knn_knode *temp = neighbors->first;
+                neighbors->first = neighbors->first->next;
+                free(temp);
+                neighbors->size--;
+            }
+        }
+    }
+
+    return neighbors;
+}
+
+static void generate_prediction(size_t k, knn_dataset dataset, float prediction[NHOURS])
+{
+    float sum = 0;
+    size_t nk, nhour;
+
+    for (nhour = 0; nhour < NHOURS; nhour++)
+    {
+        sum = 0;
+        for (nk = 0; nk < k; nk++)
+        {
+            sum += dataset.data_buff[nk][nhour] / k;
+        }
+        prediction[nhour] = sum;
+    }
+}
+
+static void save_predcition(FILE *f, float prediction[NHOURS])
+{
+    size_t nhours;
+
+    for (nhours = 0; nhours < NHOURS; nhours++)
+    {
+        if (nhours == NHOURS - 1)
+        {
+            fprintf(f, "%f\n", prediction[nhours]);
+        }
+        else
+        {
+            fprintf(f, "%f,", prediction[nhours]);
+        }
+    }
+}
+
+void knn(size_t k, knn_dataset dataset)
+{
+    FILE *file;
+    size_t ndays;
+
+    file = fopen("Prediction.txt", "w");
+    if (file == NULL)
+    {
+        perror("Unable to open file");
+        abort();
+    }
+    for (ndays = 0; ndays < NPREDICTIONS; ndays++)
+    {
+        // knn_kllist neighbors = 
+        float prediction[NHOURS];
+        generate_prediction(k, neighbors, prediction);
+        save_predcition(file, prediction);
+        // funcion de Marcos
+    }
+    fclose(file);
+}
