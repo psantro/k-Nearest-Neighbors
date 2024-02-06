a1ce9d777a0e6e303b086cc3dcb19626aef87ec3
psantro <pablosantron@gmail.com>
2024-02-07 00:03:55 +0100

    Added compute chunk size

--- src/main.c
+++ src/main.c
@@ -37,6 +37,20 @@ static int init(int argc, char **argv, struct knn_args *args)
 }
 
 /**
+ * @brief Calculates chunks size for master and slaves based on total and np;
+ *
+ * @param       total               Total of data to analize.
+ * @param       np                  Number of processes.
+ * @param[out]  master_chunk_size   Master chunk size.
+ * @param[out]  slaves_chunk_size   Slaves chunk size.
+ */
+static void calculate_chunk_size(int total, int np, int *master_size, int *slaves_size)
+{
+    *master_size = total / np + total % np;
+    *slaves_size = total / np;
+}
+
+/**
  * @brief Executes program.
  *
  * @param[in]   filename    Dataset filename.
@@ -49,9 +63,12 @@ static int exec(char const *filename, int k, int np, int nt)
 {
     float *data;
     int pid, ndays, load_ok;
+    dataset data_to_analize, data_to_predict;
+    int master_chunk_size, slaves_chunk_size;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
+    // Step 1. Master loads dataset.
     if (pid == 0)
     {
         load_ok = knn_load_dataset(filename, &ndays, &data);
@@ -62,7 +79,9 @@ static int exec(char const *filename, int k, int np, int nt)
         }
     }
 
-    MPI_Bcast(&ndays, 1, MPI_INTEGER, 0, MPI_COMM_WORLD);
+    // Step 2? Broadcast chunksize.
+    calculate_chunk_size(ndays - NPREDICTIONS, np, &master_chunk_size, &slaves_chunk_size);
+    MPI_Bcast(&slaves_chunk_size, 1, MPI_INTEGER, 0, MPI_COMM_WORLD);
 
     /**
      * @todo Make loop scattering and gathering neighbors.
